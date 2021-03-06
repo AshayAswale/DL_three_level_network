8ff19850974e2128fb067cfd255d9d04ccb62607
Ashay Aswale <asaswale@wpi.edu>
2021-03-05 21:58:14 -0500

    Submission

--- homework4_template2.py
+++ homework4_template2.py
@@ -1,12 +1,15 @@
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
 import scipy.optimize
 import copy
 import random
+from sklearn.decomposition import PCA
+from mpl_toolkits.mplot3d import Axes3D
 
 # For this assignment, assume that every hidden layer has the same number of neurons.
-NUM_HIDDEN_LAYERS = 1
+NUM_HIDDEN_LAYERS = 3
 NUM_INPUT = 784
 NUM_HIDDEN = 10
 NUM_OUTPUT = 10
@@ -96,11 +99,12 @@ def forward_prop (x, y, weightsAndBiases):
 
     zs=[]
     hs=[] 
-    
-    zs.append((np.dot( Ws[0], x).T + bs[0]).T) ######## Extra Transpose
-    hs.append(relu(zs[0]))
-    zs.append((np.dot( Ws[1], hs[0]).T + bs[1]).T) ######## Extra Transpose
-    yhat=getYHat(zs[1]) 
+    hs.append(x)
+    for i in range(NUM_HIDDEN_LAYERS):
+        zs.append((np.dot( Ws[i], hs[i]).T + bs[i]).T) ######## Extra Transpose
+        hs.append(relu(zs[i]))
+    zs.append((np.dot( Ws[-1], hs[-1]).T + bs[-1]).T) ######## Extra Transpose
+    yhat=getYHat(zs[-1]) 
 
     loss = getError(y, yhat)  #### Possible???
     
@@ -116,19 +120,80 @@ def back_prop (x, y, weightsAndBiases, alpha = 0.01):
     dJdbs = []  # Gradients w.r.t. biases   
 
     g = (yhat - y)
-    dJdWs.append( np.dot(g, hs[0].T) )
+    dJdWs.append( np.dot(g, hs[-1].T) )
     dJdbs.append( np.mean(g, axis=1) )
 
-    g = (np.dot(g.T, Ws[1])*(relu_d(zs[0]).T)).T
-    # print(zs[0])
-    dJdWs.append( np.dot(g, x.T) + alpha*Ws[0]/len(x[0]))    
-    dJdbs.append( np.mean(g, axis=1) )
+    for i in range(NUM_HIDDEN_LAYERS-1, -1, -1):
+        g = (np.dot(g.T, Ws[i+1])*(relu_d(zs[i]).T)).T
+        # print(zs[0])
+        dJdWs.append( np.dot(g, hs[i].T) + alpha*Ws[i]/len(x[0]))    
+        dJdbs.append( np.mean(g, axis=1) )
 
     dJdWs.reverse()
     dJdbs.reverse()
 
     # Concatenate gradients
     return np.hstack([ dJdW.flatten() for dJdW in dJdWs ] + [ dJdb.flatten() for dJdb in dJdbs ]) 
+
+
+def double_cross_validation(X_tr, y_tr, X_tr_vald, y_tr_vald):
+    no_data = X_tr.shape[1]
+    no_features = X_tr.shape[0]
+
+    global NUM_HIDDEN_LAYERS, NUM_HIDDEN
+
+    n_hidden_layers_set = np.array([3,4,5])
+    n_hidden_set = np.array([30,40,50])
+    eps_set = np.array([0.001, 0.005, 0.01, 0.05])
+    n_squig_set = np.array([16,32,64])
+    alpha_set = np.array([0.0])
+    epochs_set = np.array([50])
+    # 16 0.001 0.05 100
+    # n_squig_set = np.array([10000, 50000])
+    # eps_set = np.array([0.01])
+    # alpha_set = np.array([0.01])
+    # epochs_set = np.array([5,10])
+    
+
+    h_star = [n_squig_set[0], eps_set[0], alpha_set[0], epochs_set[0]]
+    A_error = math.inf
+    A_accuracy = 0
+
+    i = 0
+
+    for n_hidden_layers in n_hidden_layers_set:
+        for n_hidden in n_hidden_set:
+            for n_squig in n_squig_set:
+                for eps in eps_set:
+                    for alpha in alpha_set:
+                        for epochs in epochs_set:
+                            NUM_HIDDEN_LAYERS = n_hidden_layers
+                            NUM_HIDDEN = n_hidden
+                            weightsAndBiases = initWeightsAndBiases()
+                            # w,b = unpack(weightsAndBiases)
+                            # print(w[0].shape)
+                            # print(weightsAndBiases.shape)
+                            weightsAndBiases = training(X_tr, y_tr, weightsAndBiases, n_squig, eps, alpha, epochs)
+                            # print(test_data(X_tr_vald, y_tr_vald, weightsAndBiases))
+                            error, perct = test_data(X_tr_vald, y_tr_vald, weightsAndBiases)
+                            # print("Hyperparameters: \n","n_hidden_layers: ",n_hidden_layers,"  n_hidden: ",n_hidden,"  n_squig: ",n_squig,"  eps: ",eps,"  alpha: ",alpha,"  epochs: ",epochs)
+                            
+                            print("Error:", error, " Accuracy:", perct, "h_star:", n_hidden_layers,n_hidden,n_squig,eps,alpha,epochs, "######## case:", i)
+                            i += 1
+                            if(error<A_error and perct > A_accuracy):
+                                A_error = error
+                                A_accuracy = perct
+                                h_star = [n_squig, eps, alpha, epochs]
+                                NUM_HIDDEN = n_hidden
+                                NUM_HIDDEN_LAYERS = n_hidden_layers
+                                print("## h_star updated ##")
+    
+    print("######################")
+    print("BEST HYPERPARAMETERS")
+    print(h_star[0], h_star[1], h_star[2], h_star[3])
+    print("######################")
+    return h_star[0], h_star[1], h_star[2], h_star[3]
+    
 
 
 def stoch_grad_regression (X_tr, y_tr, weightsAndBiases):
@@ -140,10 +205,12 @@ def stoch_grad_regression (X_tr, y_tr, weightsAndBiases):
     vald_perct = 80
     vald_num = (int)(no_data*vald_perct/100)
 
+    global NUM_HIDDEN_LAYERS, NUM_HIDDEN
+
     # Step 1, random w and b generation
 
     # Randomizing the data
-    randint = (random.randint(1, 99))
+    # randint = (random.randint(1, 99))
     # random_rearrange(X_tr, y_tr, randint) #seed can be any random number
 
 
@@ -156,50 +223,51 @@ def stoch_grad_regression (X_tr, y_tr, weightsAndBiases):
     ###############################
     #### Final Training Tuning ####
     ###############################
-    # value = int (input("Enter 1 for hyperparameter tuning\nEnter 2 for training on the tuned hyperparameters\n"))
+    value = int (input("Enter 1 for hyperparameter tuning\nEnter 2 for training on the tuned hyperparameters\n"))
     
-    # if (value == 1):
-    #     print("Tuning Hyperparameters!")
-    #     n_squig, eps, alpha, epochs = double_cross_validation(X_tr, y_tr)
-    # else:
-    #     print("Training using pretuned hyperparameters")
-    n_squig, eps, alpha, epochs = 10, 0.01, 0.05, 100
+    if (value == 1):
+        print("Tuning Hyperparameters!")
+        n_squig, eps, alpha, epochs = double_cross_validation(X_tr, y_tr, X_tr_vald, y_tr_vald)
+    else:
+        print("Training using pretuned hyperparameters")
+        #  3 50 32 0.001 0.0 100
+        NUM_HIDDEN_LAYERS = 3
+        NUM_HIDDEN = 50
+        n_squig, eps, alpha, epochs = 32, 0.001, 0.0, 100
+    # n_squig, eps, alpha, epochs = 32, 0.001, 0.0, 100
 
+    weightsAndBiases = initWeightsAndBiases()    
+    weightsAndBiases, trajectory = training(X_tr, y_tr, weightsAndBiases, n_squig, eps, alpha, epochs)
+    print("FINAL Validation", test_data(X_tr_vald, y_tr_vald, weightsAndBiases))
+    # print("Validation:",training(X_tr, X_tr_vald, y_tr, y_tr_vald, weightsAndBiases, n_squig, eps, alpha, epochs))
+    return weightsAndBiases, trajectory
+
+
+def training(X_tr, y_tr, weightsAndBiases, n_squig, eps, alpha, epochs):
     no_data = X_tr.shape[1]
-
+    trajectory = []
     for epoch in range(0, epochs):
-        print("Epoch:", epoch)
-        print("Validation Error:",test_data(X_tr_vald, y_tr_vald, weightsAndBiases))
         data_remain = True
         n_curr = 0
         n_next = n_squig
         i = 0
+        # print(test_data(X_tr, y_tr, weightsAndBiases))
         while(data_remain):
-            # print(i)
             i+=1
             X_tr_temp = X_tr[:,n_curr:(min(n_next, no_data))]
-            # print(X_tr_temp.shape)
             y_tr_temp = y_tr[:,n_curr:(min(n_next, no_data))]
+
             n_curr = n_next
             n_next += n_squig
 
             data_remain = True if n_next<no_data else False
             
             dwdbs = back_prop(X_tr_temp, y_tr_temp, weightsAndBiases, alpha)
-            # ws, bs = unpack(weightsAndBiases)
-            # print(bs[0])
-            # print(b[0])
-            # print(np.mean(dwdbs))
-            # print(weightsAndBiases)
-            # print(dwdbs)
             weightsAndBiases -= eps*dwdbs
-            # print(dwdbs)
-            # print(weightsAndBiases)
-            # exit()
+        trajectory.append(weightsAndBiases)
+        print(len(trajectory))
             
-            
-            
-    return weightsAndBiases
+    return weightsAndBiases, trajectory
         
 
 def test_data(X_te, y_te, weightsAndBiases):
@@ -254,16 +322,22 @@ def plotSGDPath (trainX, trainY, trajectory):
     # along with the associated loss (cross-entropy), plus a superimposed 
     # trajectory across the landscape that was traversed using SGD. Use
     # sklearn.decomposition.PCA's fit_transform and inverse_transform methods.
+    pca = PCA(n_components=2)
+    trajectory = np.array(trajectory)
+    transWeightsAndBiases = pca.fit_transform(trajectory)
 
     def toyFunction (x1, x2):
-        return np.sin((2 * x1**2 - x2) / 10.)
+        invWeightsAndBiases = pca.inverse_transform([x1, x2])
+        loss = forward_prop(trainX, trainY, invWeightsAndBiases)[0]
+        return loss
+
 
     fig = plt.figure()
     ax = fig.gca(projection='3d')
 
     # Compute the CE loss on a grid of points (corresonding to different w).
-    axis1 = np.arange(-np.pi, +np.pi, 0.05)  # Just an example
-    axis2 = np.arange(-np.pi, +np.pi, 0.05)  # Just an example
+    axis1 = np.arange(-10, +10, 1)  # Just an example
+    axis2 = np.arange(-10, +10, 1)  # Just an example
     Xaxis, Yaxis = np.meshgrid(axis1, axis2)
     Zaxis = np.zeros((len(axis1), len(axis2)))
     for i in range(len(axis1)):
@@ -271,11 +345,11 @@ def plotSGDPath (trainX, trainY, trajectory):
             Zaxis[i,j] = toyFunction(Xaxis[i,j], Yaxis[i,j])
     ax.plot_surface(Xaxis, Yaxis, Zaxis, alpha=0.6)  # Keep alpha < 1 so we can see the scatter plot too.
 
-    # Now superimpose a scatter plot showing the weights during SGD.
-    Xaxis = 2*np.pi*np.random.random(8) - np.pi  # Just an example
-    Yaxis = 2*np.pi*np.random.random(8) - np.pi  # Just an example
-    Zaxis = toyFunction(Xaxis, Yaxis)
-    ax.scatter(Xaxis, Yaxis, Zaxis, color='r')
+    # # Now superimpose a scatter plot showing the weights during SGD.
+    # Xaxis = 2*np.pi*np.random.random(8) - np.pi  # Just an example
+    # Yaxis = 2*np.pi*np.random.random(8) - np.pi  # Just an example
+    # Zaxis = toyFunction(Xaxis, Yaxis)
+    # ax.scatter(Xaxis, Yaxis, Zaxis, color='r')
 
     plt.show()
 
@@ -323,9 +397,14 @@ if __name__ == "__main__":
     print(scipy.optimize.check_grad(lambda wab: forward_prop(np.atleast_2d(trainX[:,0:5]), np.atleast_2d(trainY[:,0:5]), wab)[0], \
                                     lambda wab: back_prop(np.atleast_2d(testX[:,0:5]), np.atleast_2d(testY[:,0:5]), wab), \
                                     weightsAndBiases))
-    stoch_grad_regression(trainX, trainY, weightsAndBiases)
+    weightsAndBiases, trajectory = stoch_grad_regression(trainX, trainY, weightsAndBiases)
+    cee_err, perct = test_data(testX, testY, weightsAndBiases)
+    print("################################")
+    print("FINAL TESTING ERROR: ",cee_err, "\nAccuracy: ", perct)
+    print("################################")
+    
 
     # weightsAndBiases, trajectory = train(trainX, trainY, weightsAndBiases, testX, testY)
     
     # # Plot the SGD trajectory
-    # plotSGDPath(trainX, trainY, ws)
+    plotSGDPath(trainX,trainY,trajectory)
